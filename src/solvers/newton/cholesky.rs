15dee39bff2e987332d68ddd534673e0c1dfaec2
Giacomo d'Antonio <giacomo.dantonio@gmail.com>
2021-02-01 18:37:47 +0100

    Implement cholesky modification algorithm

--- src/solvers/newton/cholesky.rs
+++ src/solvers/newton/cholesky.rs
@@ -1,3 +1,5 @@
+use std::f64::NEG_INFINITY;
+
 use argmin::prelude::Error;
 use nalgebra::{DMatrix, DVector};
 
@@ -6,7 +8,7 @@ static TOLERANCE : f64 = 1E-5;
 
 /// Cholesky LDL factorization, for the moment without modifications
 // (Algorithm 3.4 at page 53).
-fn factorization(mat_a: &DMatrix<f64>) -> Result<(DMatrix<f64>, DVector<f64>), Error>
+fn factorization(mat_a: &DMatrix<f64>, delta: f64, beta: f64) -> Result<(DMatrix<f64>, DVector<f64>), Error>
 {
     let (rows, cols) = mat_a.shape();
 
@@ -23,16 +25,31 @@ fn factorization(mat_a: &DMatrix<f64>) -> Result<(DMatrix<f64>, DVector<f64>), E
     for j in 0 .. rows {
         mat_c[(j, j)] =
             mat_a[(j, j)] - (0 .. j).map(|s| mat_d[s] * mat_l[(j, s)].powi(2)).sum::<f64>();
-        mat_d[j] = mat_c[(j, j)];
+
+        let mut theta_j = f64::NEG_INFINITY;
+        for i in j+1 .. rows {
+            let c_ij =
+                mat_a[(i,j)] - (0 .. j).map(|s| mat_d[s] * mat_l[(i,s)] * mat_l[(j,s)]).sum::<f64>();
+            mat_c[(i, j)] = c_ij;
+
+            theta_j = if c_ij > theta_j { c_ij } else { theta_j };
+        }
+
+        // d_j = max(|c_ij|, (theta_j / beta)^2, delta)
+        let mut d_j =  mat_c[(j, j)].abs();
+        if theta_j > f64::NEG_INFINITY && d_j < (theta_j / beta).powi(2) {
+            d_j = (theta_j / beta).powi(2);
+        }
+        if d_j < delta {
+            d_j = delta;
+        }
+        mat_d[j] = d_j;
 
         if mat_d[j] < TOLERANCE {
             return Err(Error::msg("Unable to compute the Cholesky decomposition: the input matrix is not positive definite."))
         }
 
         for i in j+1 .. rows {
-            mat_c[(i, j)] =
-                mat_a[(i,j)] - (0 .. j).map(|s| mat_d[s] * mat_l[(i,s)] * mat_l[(j,s)]).sum::<f64>();
-               
             mat_l[(i, j)] = mat_c[(i, j)] / mat_d[j];
         }
     }
@@ -44,6 +61,10 @@ fn factorization(mat_a: &DMatrix<f64>) -> Result<(DMatrix<f64>, DVector<f64>), E
 mod tests {
     use super::*;
 
+    fn is_positive_definite(mat: &DMatrix<f64>) -> bool {
+        mat.clone().cholesky().is_some()
+    }
+
     #[test]
     fn test_positive_definite() {
         let mat_a = DMatrix::from_row_slice(3, 3, &[
@@ -52,7 +73,7 @@ mod tests {
             0f64, 0f64, 1f64
         ]);
 
-        let (mat_l, vec_d) = factorization(&mat_a).unwrap();
+        let (mat_l, vec_d) = factorization(&mat_a, 1E-4, 10.0).unwrap();
         let mat_d = DMatrix::from_diagonal(&vec_d);
 
         let mat_l_t = mat_l.transpose();
@@ -61,5 +82,22 @@ mod tests {
         for entry in (actual - mat_a).iter() {
             assert!(entry.abs() < TOLERANCE);
         }
+    }
+
+    #[test]
+    fn test_indefinite() {
+        let mat_a = DMatrix::from_row_slice(3, 3, &[
+            1f64, 2f64, 0f64,
+            2f64, 1f64, 0f64,
+            0f64, 0f64, 1f64
+        ]);
+        assert!(!is_positive_definite(&mat_a));
+
+        let (mat_l, vec_d) = factorization(&mat_a, 1E-4, 10.0).unwrap();
+        let mat_d = DMatrix::from_diagonal(&vec_d);
+
+        let mat_l_t = mat_l.transpose();
+        let corrected = mat_l * mat_d * mat_l_t;
+        assert!(is_positive_definite(&corrected));
     }
 }
